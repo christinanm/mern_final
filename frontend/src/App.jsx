bde21496c73101e2b92f57e2b82e5dd04c695381
Christina Mekheil <mekheilc@gmail.com>
2025-04-25 23:19:41 -0400

    capitalizatiojn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import Signup from "./pages/signup";
+import Signup from "./pages/Signup";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Login from "./pages/Login";
